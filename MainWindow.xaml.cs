46fb0fe3dedd99f1f348e34f883644b42c1a92dd
Titile <ando.randrianari@gmail.com>
2016-04-14 14:47:16 +0200

    Added files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_game
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartNewGame(object sender, RoutedEventArgs e)
+        {
+            StartingWindow _startingWindow = new StartingWindow();
+            _startingWindow.Show();
+            this.Close();
+        }
+
+        private void LoadGame(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitGame(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
